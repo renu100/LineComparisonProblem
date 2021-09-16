7da8c8b4e9dc60424c7a50a1b2fcc8c74c180829
renu100 <renu.chandraker08@gmail.com>
2021-09-16 14:15:40 +0530

    Added: Calculate the length

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,14 @@
 package com.lineComparisons;
 
 public class LineComparison {
-public static void main(String[] args) {
-	System.out.println("Welcome");
-}
+	void line1(double x1, double x2, double y1, double y2) {
+		double value = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println(value);
+	}
+
+	public static void main(String[] args) {
+		LineComparison lineComparison = new LineComparison();
+		lineComparison.line1(5, 10, 5, 0);
+
+	}
 }
