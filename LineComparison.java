e3812e27f2ab894638581c39da00db649fc6db3d
renu100 <renu.chandraker08@gmail.com>
2021-09-16 14:41:20 +0530

    Added: Check one line is equal, greater or less than the other line

--- LineComparison.java
+++ LineComparison.java
@@ -1,19 +1,40 @@
 package com.lineComparisons;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	public static void main(String[] args) {
-		double x1 = 5, y1 = 5, x2 = 5, y2 = 0, x3 = 5, y3 = 9, x4 = 8, y4 = 0;
-		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));//line 1 distance
-		double dist1 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));//line 2 distance
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter x1 = ");
+		double x1 = scan.nextDouble();
+		System.out.println("Enter y1 = ");
+		double y1 = scan.nextDouble();
+		System.out.println("Enter x2 = ");
+		double x2 = scan.nextDouble();
+		System.out.println("Enter y2 = ");
+		double y2 = scan.nextDouble();
+		System.out.println("Enter x3 = ");
+		double x3 = scan.nextDouble();
+		System.out.println("Enter y3 = ");
+		double y3 = scan.nextDouble();
+		System.out.println("Enter x4 = ");
+		double x4 = scan.nextDouble();
+		System.out.println("Enter y4 = ");
+		double y4 = scan.nextDouble();
+		// calculating distane
+		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		double dist1 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
 		String line1 = Double.toString(dist);
 		String line2 = Double.toString(dist1);
 		Boolean equal = line1.equals(line2);
-		System.out.println(line1);
-		System.out.println(line2);
-		if (equal == true) {
+		int compare = line1.compareTo(line2);
+
+		if (compare == 0) {
 			System.out.println("Equal");
+		} else if (compare >= 1) {
+			System.out.println("Line1 Greater then Line2 ");
 		} else {
-			System.out.println("Not Equal");
+			System.out.println("line1 Smaller then Line2 ");
 		}
 	}
 }
