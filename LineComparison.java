79574ed936e271890dd22c3d0ebf814f7c05254b
renu100 <renu.chandraker08@gmail.com>
2021-09-16 14:31:09 +0530

    Added: Check equality od two line

--- LineComparison.java
+++ LineComparison.java
@@ -1,14 +1,19 @@
 package com.lineComparisons;
 
 public class LineComparison {
-	void line1(double x1, double x2, double y1, double y2) {
-		double value = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println(value);
-	}
-
 	public static void main(String[] args) {
-		LineComparison lineComparison = new LineComparison();
-		lineComparison.line1(5, 10, 5, 0);
-
+		double x1 = 5, y1 = 5, x2 = 5, y2 = 0, x3 = 5, y3 = 9, x4 = 8, y4 = 0;
+		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));//line 1 distance
+		double dist1 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));//line 2 distance
+		String line1 = Double.toString(dist);
+		String line2 = Double.toString(dist1);
+		Boolean equal = line1.equals(line2);
+		System.out.println(line1);
+		System.out.println(line2);
+		if (equal == true) {
+			System.out.println("Equal");
+		} else {
+			System.out.println("Not Equal");
+		}
 	}
 }
